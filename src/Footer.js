d4df55bb9fe14b8cc0ade2539201c7e1a816956c
Michael Allgeier <michael_allgeier@insideranken.org>
2021-12-09 15:57:25 -0600

    updated some styling

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="p-3 bg-dark text-center">
+    <footer className="p-3 bg-dark text-center border-top border-light">
       <div className="text-center">&copy; Michael Allgeier 2021</div>
       <div className="text-dark">Congrats You Found Me!</div>
     </footer>
